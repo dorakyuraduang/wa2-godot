86701984d0692c12b5e5299c41d097aaa044394e
dorakyura <1183305811@qq.com>
2025-04-24 11:52:49 +0800

    修复返回主菜单没有去除滤镜

--- script/Wa2UiMgr.cs
+++ script/Wa2UiMgr.cs
@@ -85,6 +85,7 @@ public partial class Wa2UiMgr : Control
 	public void OpenTitleMenu()
 	{
 		_engine.Reset();
+		_engine.GameSav.Reset();
 		_engine.SubViewport.Hide();
 		_engine.State = Wa2EngineMain.GameState.TITLE;
 		TitleMenu.Open();
